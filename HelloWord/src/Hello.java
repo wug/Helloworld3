8058c13b7fde99f03d5f8e7abccd29ab8e979bae
wg <wuguang125234@163.com>
2011-07-21 18:38:59 +0800

    add new line
    Signed-off-by: wg <wuguang125234@163.com>

--- HelloWord/src/Hello.java
+++ HelloWord/src/Hello.java
@@ -6,7 +6,8 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello World! haha");
+		System.out.println("good");
 
 	}
 
